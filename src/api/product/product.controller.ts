1853ad007dd4b3f7977771e6a329696f106c140e
Jeewapadma <udithjeewapadma117@gmail.com>
2025-07-21 13:39:53 +0530

    generate rest api to user

--- src/api/product/product.controller.ts
+++ src/api/product/product.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  allProducts(): string{
+    return "products";
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './api/product/product.module';
+import { UserModule } from './api/user/user.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
