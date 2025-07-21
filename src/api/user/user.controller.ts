44a99299e78518193074b8989050516b0032b381
Jeewapadma <udithjeewapadma117@gmail.com>
2025-07-21 13:55:16 +0530

    implementing get endpoint to user

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-}
+
+  @Get()
+    getAllUsers(): {allUsers:string} {
+      return {
+        allUsers:'all',
+      }
+    }
+  }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductModule } from './api/product/product.module';
 import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [ProductModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
