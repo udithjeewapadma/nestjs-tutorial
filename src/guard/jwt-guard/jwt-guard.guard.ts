40f0137e3e197bbb03bd51c25590ddff4f75edd0
Jeewapadma <udithjeewapadma117@gmail.com>
2025-08-14 14:55:53 +0530

    implementing reflector method

--- src/guard/jwt-guard/jwt-guard.guard.ts
+++ src/guard/jwt-guard/jwt-guard.guard.ts
@@ -1,15 +1,28 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import {Request} from 'express'
 import { JwtAuthService } from 'src/config/jwt/jwt.service';
+import { PUBLIC_DEC_KEY } from 'src/decorator/public/public.decorator';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
 
-  constructor(private readonly JwtService: JwtAuthService){
+  constructor(
+    private readonly JwtService: JwtAuthService,
+    private readonly reflector: Reflector ){
   }
   async canActivate(
     context: ExecutionContext,
   ): Promise<boolean>{
+
+    const isPublic = this.reflector.getAllAndOverride<boolean>(PUBLIC_DEC_KEY,[
+      context.getHandler(),
+      context.getClass()
+    ]);
+
+    if(isPublic) return true;
+
+
     const req:Request = context.switchToHttp().getRequest();
     const tokenstring = req.headers.authorization;
     console.log(tokenstring);
